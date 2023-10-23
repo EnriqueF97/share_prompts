b121f3272585e73b33701913acf0de59ed326776
Enrique Favila <kike.fvla@gmail.com>
2023-10-22 20:10:27 -0600

    Prepare for build deployment

--- app/page.jsx
+++ app/page.jsx
@@ -3,16 +3,16 @@ import Feed from "@components/Feed";
 const Home = () => (
   <section className="w-full flex-center flex-col">
     <h1 className="head_text text-center">
-      Discover & Share
+      Discover & write your own
       <br className="max-md:hidden" />
       <span className="orange_gradient text-center">
-        {" "}
-        AI-Powered Prompts
+        AI-Useful Prompts
       </span>
     </h1>
     <p className="desc text-center">
-      Promptopia is an open-source AI prompting tool for modern world
-      to discover, create and share creative prompts
+      With this app, you will be able to see useful AI Prompts to be
+      used on ChatGPT & Google Bard from other users, create your own
+      and share them with the world!
     </p>
 
     <Feed />
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        satoshi: ["Satoshi", "sans-serif"],
-        inter: ["Inter", "sans-serif"],
+        satoshi: ["sans-serif"],
+        inter: ["sans-serif"],
       },
       colors: {
         "primary-orange": "#FF5722",
